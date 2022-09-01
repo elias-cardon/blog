68b13654d3a0152e59236f84ffd0033031eee752
Elias Cardon <62133614+elias-cardon@users.noreply.github.com>
2022-09-01 18:22:36 +0200

    On met les meta tags

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="C'est le blog de Jobba, le meilleur de France.">
+    <meta name="keywords" content="HTML, CSS, JavaScript, blog, Jobba, meilleur, Elias, Menace, Article, Star Wars">
+    <meta name="author" content="Elias Cardon aka Jobba">
+    <title>Le blog de Jobba</title>
+</head>
+<body>
+
+</body>
+</html>
