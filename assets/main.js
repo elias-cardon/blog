834e277cbed4203a39fb063f5248b791dc6e3f96
Elias Cardon <62133614+elias-cardon@users.noreply.github.com>
2022-09-04 14:46:21 +0200

    On fait le media queries du navbar (partie JS)

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,9 @@
 const navItems = document.querySelector('.nav__items');
 const openNavBtn = document.querySelector('#open__nav-btn');
-const closeNavBtn = document.querySelector('');
+const closeNavBtn = document.querySelector('#close__nav-btn');
+
+
+const openNav = () =>{
+
+}
+openNavBtn.addEventListener('click', openNavBtn);
