3f7777d3407151bcb4bfe171012b0bc1f19af6b3
Elias Cardon <62133614+elias-cardon@users.noreply.github.com>
2022-09-05 16:24:01 +0200

    On finit le media queries du navbar

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
             <li><a href="about.php">A propos</a></li>
             <li><a href="services.php">Services</a></li>
             <li><a href="contact.php">Contact</a></li>
-            <li><a href="signin.php">Se connecter</a></li>
+            <!--<li><a href="signin.php">Se connecter</a></li>-->
             <li class="nav__profile">
                 <div class="avatar">
                     <img src="./assets/images/avatar1.jpg" alt="Un avatar">
