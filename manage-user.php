2ad4404655dd1b3ff6e9e262dd2d0eeda54a2661
Elias Cardon <62133614+elias-cardon@users.noreply.github.com>
2022-09-15 10:56:28 +0200

    On fait la liste des utilisateurs

---
+++ manage-user.php
@@ -0,0 +1,187 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="C'est le blog de Jobba, le meilleur de France.">
+    <meta name="keywords" content="HTML, CSS, JavaScript, blog, Jobba, meilleur, Elias, Menace, Article, Star Wars">
+    <meta name="author" content="Elias Cardon aka Jobba">
+    <title>Le blog de Jobba</title>
+    <!--C'est le CSS-->
+    <link rel="icon" type="image/png" href="assets/images/logo.png"/>
+    <link rel="stylesheet" href="assets/style.css">
+    <!--Iconscout CDN-->
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+    <!--Google Fonts-->
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet">
+</head>
+<body>
+<!--==============================NAVBAR=========================================-->
+<nav>
+    <div class="container nav__container">
+        <a href="index.php" class="nav__logo">Le blog de Jobba</a>
+        <ul class="nav__items">
+            <li><a href="blog.php">Blog</a></li>
+            <li><a href="about.php">A propos</a></li>
+            <li><a href="services.php">Services</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            <!--<li><a href="signin.php">Se connecter</a></li>-->
+            <li class="nav__profile">
+                <div class="avatar">
+                    <img src="./assets/images/avatar1.jpg" alt="Un avatar">
+                </div>
+                <ul>
+                    <li><a href="dashboard.php">Dashboard</a></li>
+                    <li><a href="logout.php">Déconnexion</a></li>
+                </ul>
+            </li>
+        </ul>
+
+        <button id="open__nav-btn"><i class="uil uil-bars"></i></button>
+        <button id="close__nav-btn"><i class="uil uil-multiply"></i></button>
+    </div>
+</nav>
+<!--==============================END OF NAVBAR=========================================-->
+<section class="dashboard">
+    <div class="container dashboard__container">
+        <button id="show__sidebar-btn" class="sidebar__toggle"><i class="uil uil-angle-right-b"></i></button>
+        <button id="hide__sidebar-btn" class="sidebar__toggle"><i class="uil uil-angle-left-b"></i></button>
+        <aside>
+            <ul>
+                <li>
+                    <a href="add-post.php">
+                        <i class="uil uil-pen"></i>
+                        <h5>Ajouter un article</h5>
+                    </a>
+                </li>
+                <li>
+                    <a href="dashboard.php">
+                        <i class="uil uil-postcard"></i>
+                        <h5>Liste des articles</h5>
+                    </a>
+                </li>
+                <li>
+                    <a href="add-user.php">
+                        <i class="uil uil-user-plus"></i>
+                        <h5>Ajouter un utilisateur</h5>
+                    </a>
+                </li>
+                <li>
+                    <a href="manage-user.php" class="active">
+                        <i class="uil uil-users-alt"></i>
+                        <h5>Liste des utilisateurs</h5>
+                    </a>
+                </li>
+                <li>
+                    <a href="add-category.php">
+                        <i class="uil uil-edit"></i>
+                        <h5>Ajouter une catégorie</h5>
+                    </a>
+                </li>
+                <li>
+                    <a href="manage-category.php">
+                        <i class="uil uil-list-ul"></i>
+                        <h5>Liste des catégories</h5>
+                    </a>
+                </li>
+            </ul>
+        </aside>
+        <main>
+            <h2>Liste des utilisateurs</h2>
+            <table>
+                <thead>
+                <tr>
+                    <th>Nom</th>
+                    <th>Pseudo</th>
+                    <th>Modifier</th>
+                    <th>Supprimer</th>
+                    <th>Admin</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>Elias Cardon</td>
+                    <td>Jobba</td>
+                    <td><a href="edit-user.php" class="btn sm">Modifier</a></td>
+                    <td><a href="delete-user.php" class="btn sm danger">Supprimer</a></td>
+                    <td>Oui</td>
+                </tr>
+                <tr>
+                    <td>Mike</td>
+                    <td>Rocky Mike</td>
+                    <td><a href="edit-user.php" class="btn sm">Modifier</a></td>
+                    <td><a href="delete-user.php" class="btn sm danger">Supprimer</a></td>
+                    <td>Non</td>
+                </tr>
+                <tr>
+                    <td>Marine</td>
+                    <td>Le Pen</td>
+                    <td><a href="edit-user.php" class="btn sm">Modifier</a></td>
+                    <td><a href="delete-user.php" class="btn sm danger">Supprimer</a></td>
+                    <td>Non</td>
+                </tr>
+                </tbody>
+            </table>
+        </main>
+    </div>
+</section>
+<!--==============================FOOTER=========================================-->
+<footer>
+    <div class="footer__socials">
+        <a href="https://www.instagram.com/el_eliohs/" target="_blank"><i class="uil uil-instagram"></i></i></a>
+        <a href="https://www.linkedin.com/in/elias-cardon-693a31a2/" target="_blank"><i class="uil uil-linkedin"></i></i></a>
+        <a href="https://github.com/elias-cardon" target="_blank"><i class="uil uil-github"></i></i></a>
+    </div>
+    <div class="container footer__container">
+        <article>
+            <h4>Catégories</h4>
+            <ul>
+                <li><a href="">Art</a></li>
+                <li><a href="">Wild Life</a></li>
+                <li><a href="">Travel</a></li>
+                <li><a href="">Science & Technologie</a></li>
+                <li><a href="">Musique</a></li>
+                <li><a href="">Nourriture</a></li>
+            </ul>
+        </article>
+        <article>
+            <h4>Support</h4>
+            <ul>
+                <li><a href="">Support en ligne</a></li>
+                <li><a href="">Numéro d'appel</a></li>
+                <li><a href="">Email</a></li>
+                <li><a href="">Support social</a></li>
+                <li><a href="">Location</a></li>
+            </ul>
+        </article>
+        <article>
+            <h4>Blog</h4>
+            <ul>
+                <li><a href="">CGU</a></li>
+                <li><a href="">Articles récents</a></li>
+                <li><a href="">A la une</a></li>
+                <li><a href="">Catégories</a></li>
+            </ul>
+        </article>
+        <article>
+            <h4>Permalinks</h4>
+            <ul>
+                <li><a href="">Accueil</a></li>
+                <li><a href="">Blog</a></li>
+                <li><a href="">A propos</a></li>
+                <li><a href="">Services</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+        </article>
+    </div>
+    <div class="footer__copyright">
+        <small>Copyright &copy; <script>document.write(new Date().getFullYear())</script> Elias Cardon</small>
+    </div>
+</footer>
+<!--==============================END FOOTER=========================================-->
+<script src="./assets/main.js"></script>
+</body>
+</html>
