b84ec6e9139417b0c24a4ded56f5d5eea542569a
Elias Cardon <62133614+elias-cardon@users.noreply.github.com>
2024-06-12 18:28:31 +0200

    On check le statut connecté

--- backend/admin/partials/header.php
+++ backend/admin/partials/header.php
@@ -1,11 +1,9 @@
 <?php
 require '../admin/config/database.php';
-//Fetch current user from database
-if (isset($_SESSION['user-id'])){
-    $id = filter_var($_SESSION['user-id'], FILTER_SANITIZE_NUMBER_INT);
-    $query = "SELECT avatar FROM users WHERE id = $id";
-    $result = mysqli_query($connection, $query);
-    $avatar = mysqli_fetch_assoc($result);
+// check login status
+if (!isset($_SESSION['user-id'])) {
+    header('location: ' . ROOT_URL . 'signin.php');
+    die();
 }
 ?>
 
