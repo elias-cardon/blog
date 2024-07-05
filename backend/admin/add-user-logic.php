88592a4105fd9b50b4b3a5c9230f973b4a83bc19
Elias Cardon <62133614+elias-cardon@users.noreply.github.com>
2024-07-05 10:07:54 +0200

    Ajout des auteurs/admin via panel admin finit

--- backend/admin/add-user-logic.php
+++ backend/admin/add-user-logic.php
@@ -2,7 +2,7 @@
 require 'config/database.php';
 
 //get form data if submit button clicked
-if (isset($_POST['submit'])){
+if (isset($_POST['submit'])) {
     $firstname = filter_var($_POST['firstname'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
     $lastname = filter_var($_POST['lastname'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
     $username = filter_var($_POST['username'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
@@ -13,34 +13,32 @@ if (isset($_POST['submit'])){
     $avatar = $_FILES['avatar'];
 
     //validate input values
-    if (!$firstname){
+    if (!$firstname) {
         $_SESSION['add-user'] = "Veuillez renseigner le prénom.";
-    } elseif (!$lastname){
+    } elseif (!$lastname) {
         $_SESSION['add-user'] = "Veuillez renseigner le nom.";
-    }elseif (!$username){
+    } elseif (!$username) {
         $_SESSION['add-user'] = "Veuillez renseigner le pseudonyme.";
-    }elseif (!$email){
+    } elseif (!$email) {
         $_SESSION['add-user'] = "Veuillez renseigner une adresse email valide.";
-    }elseif (!$is_admin){
-        $_SESSION['add-user'] = "Veuillez sélectionner le rôle de l'utilisateur.";
-    }elseif (strlen($createpassword) < 8 || strlen($confirmpassword) < 8){
+    } elseif (strlen($createpassword) < 8 || strlen($confirmpassword) < 8) {
         $_SESSION['add-user'] = "Le mot de passe doit contenir plus de 8 caractères.";
-    }elseif (!$avatar['name']){
+    } elseif (!$avatar['name']) {
         $_SESSION['add-user'] = "Veuillez ajouter un avatar.";
     } else {
         //Check if password don't match
-        if ($createpassword !== $confirmpassword){
+        if ($createpassword !== $confirmpassword) {
             $_SESSION['add-user'] = "Les mots de passe ne correspondent pas.";
-        } else{
+        } else {
             //hash password
             $hashed_password = password_hash($createpassword, PASSWORD_DEFAULT);
 
             //check if username or email already exit in database
             $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email'";
             $user_check_result = mysqli_query($connection, $user_check_query);
-            if (mysqli_num_rows($user_check_result) > 0){
+            if (mysqli_num_rows($user_check_result) > 0) {
                 $_SESSION['add-user'] = "Pseudonyme ou adresse email déjà existant";
-            } else{
+            } else {
                 //Work on avatar
                 //rename avatar
                 $time = time(); //make each image unique using current timestamp
@@ -52,39 +50,42 @@ if (isset($_POST['submit'])){
                 $allowed_files = ['png', 'jpg', 'jpeg'];
                 $extention = explode('.', $avatar_name);
                 $extention = end($extention);
-                if(in_array($extention, $allowed_files)){
+                if (in_array($extention, $allowed_files)) {
                     //Make sure the file is not too large (1Mo)
-                    if ($avatar['size'] < 1000000){
+                    if ($avatar['size'] < 1000000) {
                         //Upload avatar
                         move_uploaded_file($avatar_tmp_name, $avatar_destination_path);
-                    }else {
-                        $_SESSION['signup'] = 'Ce fichier est trop volumineux. Il doit faire moins de 1Mo.';
+                    } else {
+                        $_SESSION['add-user'] = 'Votre avatar est trop volumineux. Il doit faire moins de 1Mo.';
                     }
-                } else{
-                    $_SESSION['signup'] = "Ce fichier doit être un JPG, un JPEG ou un PNG.";
+                } else {
+                    $_SESSION['add-user'] = "Votre avatar doit être un JPG, un JPEG ou un PNG.";
                 }
             }
         }
     }
     //redirect back to signup if any problem
-    if (isset($_SESSION['signup'])){
+    if (isset($_SESSION['add-user'])) {
         //pass the form data back to signup page
-        $_SESSION['signup-data'] = $_POST;
-        header('location:'.ROOT_URL.'signup.php');
+        $_SESSION['add-user-data'] = $_POST;
+        header('location:' . ROOT_URL . 'backend/admin/add-user.php');
         die();
     } else {
         //insert new user into users table
-        $insert_user_query = "INSERT INTO users (firstname, lastname, username, email, password, avatar, is_admin) VALUES ('$firstname', '$lastname', '$username', '$email', '$hashed_password', '$avatar_name', 0)";
+        $insert_user_query = "INSERT INTO users SET firstname='$firstname',
+              lastname='$lastname', username='$username', email='$email', password='$hashed_password',
+              avatar='$avatar_name', is_admin='$is_admin'";
+
         $insert_user_result = mysqli_query($connection, $insert_user_query);
 
-        if (!mysqli_errno($connection)){
+        if (!mysqli_errno($connection)) {
             //Redirect to signup pag with success message
-            $_SESSION['signup-success'] = "Inscription réussie. Vous pouvez vous connecter.";
-            header('location:'.ROOT_URL.'signin.php');
+            $_SESSION['add-user-success'] = "$firstname $lastname a bien été ajouté.";
+            header('location:' . ROOT_URL . 'backend/admin/manage-user.php');
         }
     }
-}else{
+} else {
     //if button not clicked, return to signup page
-    header('location:'.ROOT_URL.'signup.php');
+    header('location:' . ROOT_URL . 'backend/admin/add-user.php');
     die();
 }
--- backend/admin/add-user.php
+++ backend/admin/add-user.php
@@ -1,20 +1,39 @@
 <?php
 require './partials/header.php';
+
+//get back form data if  there was an error
+$firstname = $_SESSION['add-user-data']['firstname'] ?? null;
+$lastname = $_SESSION['add-user-data']['lastname'] ?? null;
+$username = $_SESSION['add-user-data']['username'] ?? null;
+$email = $_SESSION['add-user-data']['email'] ?? null;
+$createpassword = $_SESSION['add-user-data']['createpassword'] ?? null;
+$confirm_password = $_SESSION['add-user-data']['confirmpassword'] ?? null;
+
+//delete session data
+unset($_SESSION['add-user-data']);
 ?>
 <body>
 <section class="form__section">
     <div class="container form__section-container">
         <h2>Ajout d'utilisateur</h2>
-        <div class="alert__message error">
-            <p>C'est un message d'erreur.</p>
-        </div>
+        <?php
+        if (isset($_SESSION['add-user'])) :
+        ?>
+            <div class="alert__message error">
+                <p>
+                    <?= $_SESSION['add-user'];
+                    unset($_SESSION['add-user']);
+                    ?>
+                </p>
+            </div>
+        <?php endif?>
         <form action="<?= ROOT_URL ?>backend/admin/add-user-logic.php" method="POST" enctype="multipart/form-data">
-            <input type="text" name="firstname" placeholder="Prénom">
-            <input type="text" name="lastname" placeholder="Nom de famille">
-            <input type="text" name="username" placeholder="Pseudonyme">
-            <input type="email" name="email" placeholder="Adresse email">
-            <input type="password" name="createpassword" placeholder="Mot de passe">
-            <input type="password" name="confirmpassword" placeholder="Confirmation du mot de passe">
+            <input type="text" name="firstname" value="<?= $firstname ?>" placeholder="Prénom">
+            <input type="text" name="lastname" value="<?= $lastname ?>" placeholder="Nom de famille">
+            <input type="text" name="username" value="<?= $username ?>" placeholder="Pseudonyme">
+            <input type="email" name="email" value="<?= $email ?>" placeholder="Adresse email">
+            <input type="password" name="createpassword" value="<?= $createpassword ?>" placeholder="Mot de passe">
+            <input type="password" name="confirmpassword" value="<?= $confirm_password ?>" placeholder="Confirmation du mot de passe">
             <select name="userrole">
                 <option value="0">Auteur</option>
                 <option value="1">Admin</option>
