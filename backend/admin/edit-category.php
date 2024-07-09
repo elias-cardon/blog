bb39140475775d5fef0cb5ab19137d419e77fb04
Elias Cardon <62133614+elias-cardon@users.noreply.github.com>
2024-07-09 12:28:23 +0200

    Modifiction des catégories en cours

--- backend/admin/edit-category.php
+++ backend/admin/edit-category.php
@@ -1,14 +1,29 @@
 <?php
 require './partials/header.php';
+
+if (isset($_GET['id'])){
+    $id = filter_var($_GET['id'], FILTER_SANITIZE_NUMBER_INT);
+
+    //fetch category from database
+    $query = "SELECT * FROM categories WHERE id = $id";
+    $result = mysqli_query($connection, $query);
+    if (mysqli_num_rows($result) == 1) {
+        $category = mysqli_fetch_assoc($result);
+    }
+} else {
+    header('location: ' . ROOT_URL . 'backend/admin/manage-category.php');
+    die();
+}
 ?>
 <body>
 <section class="form__section">
     <div class="container form__section-container">
         <h2>Modifier la catégorie</h2>
-        <form action="" enctype="multipart/form-data">
-            <input type="text" placeholder="Nom de la catégorie">
-            <textarea rows="4" placeholder="Description"></textarea>
-            <button type="submit" class="btn">Modifier la catégorie</button>
+        <form action="<?= ROOT_URL ?>backend/admin/edit-category-logic.php" method="POST">
+            <input type="hidden" name="id" value="<?= $category['title'] ?>" placeholder="Nom de la catégorie">
+            <input type="text" name="title" value="<?= $category['title'] ?>" placeholder="Nom de la catégorie">
+            <textarea rows="4" name="description" placeholder="Description"><?= $category['description'] ?></textarea>
+            <button type="submit" name="submit" class="btn">Modifier la catégorie</button>
         </form>
     </div>
 </section>
