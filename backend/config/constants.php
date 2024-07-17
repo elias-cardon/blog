4bd1c61a474b3258d5b5f6f31ce0e203efb93ba1
Elias Cardon <62133614+elias-cardon@users.noreply.github.com>
2024-07-18 00:23:00 +0200

    On change le mot de passe

--- backend/config/constants.php
+++ backend/config/constants.php
@@ -3,5 +3,5 @@ session_start();
 define('ROOT_URL', 'http://localhost/blog/');
 define('DB_HOST', 'localhost');
 define('DB_USER', 'jobba');
-define('DB_PASS', '9i51o9vT?');
+define('DB_PASS', 'admin1234');
 define('DB_NAME', 'blog');
